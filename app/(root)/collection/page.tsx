29f7b486e39ffe92fe529137e31a6baee67fe34d
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-10-25 02:29:06 +0530

    create user on our db

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-
 const page = () => {
-  return <div>col page</div>;
+  return <div>page</div>;
 };
-
 export default page;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/navigation/Header";
 import LeftSidebar from "@/components/navigation/LeftSidebar";
-import RightSidebar from "@/components/navigation/RightSidebar";
+// import RightSidebar from "@/components/navigation/RightSidebar";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,7 +11,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        <RightSidebar />
+        RightSidebar
       </div>
     </main>
   );
