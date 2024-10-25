feb42bdbcc06f68d6296971e432e05f253023570
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-10-25 13:26:44 +0530

    right sidebar and tags

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp signInUrl="/sign-in" fallbackRedirectUrl={"/"} />;
+  return <SignUp signInFallbackRedirectUrl={"/"} signInUrl="/sign-in" />;
 }
